df8632f6888cf264b967fdbd16f84f1f41ce330e
ppavlidis92 <116918115+ppavlidis92@users.noreply.github.com>
2024-08-23 17:32:35 +0300

    implemented currentIndex for requesting media and user for each post seperately for posts and fixed front to show 1 post every 6 sec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,64 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Typography } from "@mui/material";
 import Image from "next/image";
 import styles from "./page.module.css";
-import usePosts from "./components/usePosts";
+import useFetchPostsData from "./components/useFetchPostsData";
 
 const Home = () => {
-  const { posts } = usePosts();
+  const { posts, currentIndex, setCurrentIndex, setOffset } = useFetchPostsData();
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      const intervalId = setInterval(() => {
+        if (currentIndex < posts.length - 1) {
+          setCurrentIndex((prevIndex) => prevIndex + 1);
+        } else {
+          // When the last post is displayed, load the next set of posts
+          setOffset((prevOffset) => prevOffset + 1);
+        }
+      }, 6000); // Display each post for 6 seconds
+
+      return () => clearInterval(intervalId);
+    }
+  }, [currentIndex, posts.length, setCurrentIndex, setOffset]);
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <div>
-          {posts && posts.length > 0 ? (
-            posts.map((post, index) => (
-              <div key={index}>
-                {post.title ? (
-                  <Typography variant="h6">{post.title}</Typography>
-                ) : (
-                  <Typography variant="h6">Untitled</Typography>
-                )}
-                {post.description ? (
-                  <Typography variant="body2">{post.description}</Typography>
-                ) : (
-                  <Typography variant="body2">No Description</Typography>
-                )}
-                {post.media && (
-                  <div>
-                    <Image
-                      src={post.media.urls.thumb}
-                      alt={`Media for ${post.title}`}
-                      width={100}
-                      height={100}
-                    />
-                    <Typography>{`Likes: ${post.media.statistics.likes}`}</Typography>
-                  </div>
-                )}
-                {post.userData && (
-                  <div>
-                    <Typography variant="body2">{`Posted by: ${post.userData.first_name} ${post.userData.last_name}`}</Typography>
-                  </div>
-                )}
-              </div>
-            ))
+          {posts && posts.length > 0 && posts[currentIndex] ? (
+            <div>
+              {posts[currentIndex].title ? (
+                <Typography variant="h6">{posts[currentIndex].title}</Typography>
+              ) : (
+                <Typography variant="h6">Untitled</Typography>
+              )}
+              {posts[currentIndex].description ? (
+                <Typography variant="body2">{posts[currentIndex].description}</Typography>
+              ) : (
+                <Typography variant="body2">No Description</Typography>
+              )}
+              {posts[currentIndex].media && (
+                <div>
+                  <Image
+                    src={posts[currentIndex].media.urls.thumb}
+                    alt={`Media for ${posts[currentIndex].title}`}
+                    width={100}
+                    height={100}
+                  />
+                  <Typography>{`Likes: ${posts[currentIndex].media.statistics.likes}`}</Typography>
+                </div>
+              )}
+              {posts[currentIndex].userData && (
+                <div>
+                  <Typography variant="body2">{`Posted by: ${posts[currentIndex].userData.first_name} ${posts[currentIndex].userData.last_name}`}</Typography>
+                </div>
+              )}
+            </div>
           ) : (
-            <Typography>No posts available</Typography>
+            <Typography>Loading...</Typography>
           )}
         </div>
       </div>
